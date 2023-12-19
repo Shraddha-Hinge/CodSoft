837ebfd45dd181d6ffe0218ab74a8e11f5543f6b
Shraddha-Hinge <154316531+Shraddha-Hinge@users.noreply.github.com>
2023-12-19 19:41:07 +0530

    Add files via upload

---
+++ CodSoft/simple_calculator.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    char operation;
+
+    // Input
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // Choose operation
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> operation;
+
+    // Perform operation and display result
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << num1 << " + " << num2 << " = " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << num1 << " - " << num2 << " = " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << num1 << " / " << num2 << " = " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operation. Please choose +, -, *, or /." << endl;
+    }
+
+    return 0;
+}
+
